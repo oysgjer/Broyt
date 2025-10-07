28b4ecb76220f65abcfb4318590b72eb030024c8
oysgjer <oysgjer@gmail.com>
2025-10-07 07:28:55 +0200

    Add files via upload

---
+++ docs/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'broyterute-v8-2-local-2025-10-07';
+const CORE = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k))))); self.clients.claim();});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
