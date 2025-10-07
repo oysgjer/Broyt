66a3e9647caf856673de13ebcfd7933b2dda1a65
oysgjer <oysgjer@gmail.com>
2025-10-07 11:04:40 +0200

    Add files via upload

---
+++ docs/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'broyterute-v8-4-2025-10-07';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => {
+      if (k !== CACHE_NAME && k.startsWith('broyterute-')) return caches.delete(k);
+    }))).then(() => self.clients.claim())
+  );
+});
+
+// Cache-first for same-origin GET
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+  event.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
